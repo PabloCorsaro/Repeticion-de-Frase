1114466b4e3875c5dd4ef1f2cedb0a0a50b6d683
PabloCorsaro <pablo.gl.corsaro@gmail.com>
2017-04-29 11:38:00 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+Repeticion de Frase
+Corsaro Pablo
+28-04-2017
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    for(int i = 0; i < 200 ; i++)
+    {
+        cout << "Quiero comer." << endl;
+    }
+
+    return 0;
+}
